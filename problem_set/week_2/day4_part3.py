91ff09d4ec285cc996da11cc4d74d7febe3bba4b
joanga <joan.marcanov@gmail.com>
2018-07-23 11:51:46 -0600

    update from work

---
+++ problem_set/week_2/day4_part3.py
@@ -0,0 +1,40 @@
+#Part_1 of the excersie
+
+# list_1 = input('Enter a set of numbers separated by commas: ').split(',')
+# list_2 = input('Enter a second set of number separated by commas: ').split(',')
+#
+#
+# list_1_set = set(list_1)
+# list_2_set = set(list_2)
+#
+# common_num = list_1_set.intersection(list_2_set)
+#
+# print (common_num)
+
+#PART #2
+# word_list = input('Enter a set of words separated by commas: ').split(',')
+#
+# unique_words = set(word_list)
+#
+# print(unique_words)
+
+
+#Part 3
+
+vocabulary = []
+user_input = 0
+while user_input != 'q':
+    if user_input != 'v':
+        input_word = input("Enter a word to add to vocabulary: ")
+        if input_word != 'v':
+            vocabulary.append(input_word)
+            user_input = input_word
+        else:
+            user_input = input_word
+
+    elif input_word == 'v':
+        print (vocabulary)
+
+        input_word = input("Enter a word to add to vocabulary: ")
+        vocabulary.append(input_word)
+        user_input = input_word
